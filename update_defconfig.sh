aa3e9483580cc6ffe6de5b958531630b9e1d3c85
Alin Jerpelea <alin.jerpelea@sony.com>
2018-09-13 11:04:00 +0200

    add the option to build devices with FB or DRM
    There are platforms that can use the DRM
    
    NOTE:
    To switch a platfrom from FB to DRM the DRM=false flag has to be
    changed to DRM=true
    
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- update_defconfig.sh
+++ update_defconfig.sh
@@ -12,6 +12,7 @@ NILE="discovery pioneer"
 TAMA="akari apollo"
 
 PLATFORMS="loire tone yoshino nile tama"
+
 cd $KERNEL_TOP/kernel
 
 for platform in $PLATFORMS; do \
@@ -19,19 +20,19 @@ for platform in $PLATFORMS; do \
 case $platform in
 loire)
     DEVICE=$LOIRE;
-    DTBO="false";;
+    DRM="false";;
 tone)
     DEVICE=$TONE;
-    DTBO="false";;
+    DRM="false";;
 yoshino)
     DEVICE=$YOSHINO;
-    DTBO="false";;
+    DRM="false";;
 nile)
     DEVICE=$NILE;
-    DTBO="false";;
+    DRM="false";;
 tama)
     DEVICE=$TAMA;
-    DTBO="true";;
+    DRM="true";;
 esac
 
 for device in $DEVICE; do \
@@ -40,13 +41,26 @@ for device in $DEVICE; do \
     echo "================================================="
     echo "Platform -> ${platform} :: Device -> $device"
     echo "Running scripts/kconfig/merge_config.sh ..."
-    ret=$(ARCH=arm64 O=${KERNEL_TMP} scripts/kconfig/merge_config.sh \
-    ${KERNEL_CFG}/base_${platform}_common_defconfig \
-    ${KERNEL_CFG}/base_${platform}"_"${device}\_defconfig \
-    ${KERNEL_CFG}/android-base.config \
-    ${KERNEL_CFG}/android-base-arm64.config \
-    ${KERNEL_CFG}/android-recommended.config \
-    ${KERNEL_CFG}/android-extra.config 2>&1);
+    if [ $DRM = "true" ]; then
+        ret=$(ARCH=arm64 O=${KERNEL_TMP} scripts/kconfig/merge_config.sh \
+            ${KERNEL_CFG}/base_drm_defconfig \
+            ${KERNEL_CFG}/base_${platform}_common_defconfig \
+            ${KERNEL_CFG}/base_${platform}"_"${device}\_defconfig \
+            ${KERNEL_CFG}/android-base.config \
+            ${KERNEL_CFG}/android-base-arm64.config \
+            ${KERNEL_CFG}/android-recommended.config \
+            ${KERNEL_CFG}/android-extra.config 2>&1);
+    else
+        ret=$(ARCH=arm64 O=${KERNEL_TMP} scripts/kconfig/merge_config.sh \
+            ${KERNEL_CFG}/base_fb_defconfig \
+            ${KERNEL_CFG}/base_${platform}_common_defconfig \
+            ${KERNEL_CFG}/base_${platform}"_"${device}\_defconfig \
+            ${KERNEL_CFG}/android-base.config \
+            ${KERNEL_CFG}/android-base-arm64.config \
+            ${KERNEL_CFG}/android-recommended.config \
+            ${KERNEL_CFG}/android-extra.config 2>&1);
+    fi
+
     case "$ret" in
         *"error"*|*"ERROR"*) echo "ERROR: $ret"; exit 1;; 
     esac
