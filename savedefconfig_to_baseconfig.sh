d148431c15e383980de01da52a9a9389fbb54074
Pavel Dubrova <pashadubrova@gmail.com>
2023-12-19 01:06:53 +0200

    savedefconfig_to_baseconfig.sh: Replace comm with grep
    Replace comm with grep. This gives us the advantage
    that we can remove duplicates even without sorting
    the fragments in alphabetical order.
    
    Also, having sorting in the "savedefconfig style" is
    much more convenient, because after many years of
    working with it, you already subconsciously know
    where the entry you need is located.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- savedefconfig_to_baseconfig.sh
+++ savedefconfig_to_baseconfig.sh
@@ -8,12 +8,8 @@ echo "Processing file: $1"
 echo "Output file: $2"
 
 if [ -f "$1" ]; then
-    cat android-base.config \
-        gki_defconfig && \
-        /tmp/android_recommended && \
-    sort -o /tmp/android_recommended /tmp/android_recommended && \
-    comm -1 -3 /tmp/android_recommended <(sort $1) > $2 && \
-    rm /tmp/android_recommended && \
+    grep -vf android-base.config $1 > $2 && \
+    grep -vf gki_defconfig $1 > $2 && \
     echo "$2 is ready!"
 else
     echo "File '$1' does not exist."
