dd33dd2d2085d8b7a957b9436a59dfa19fb04b8a
Pavel Dubrova <pashadubrova@gmail.com>
2024-11-24 06:11:49 +0200

    Remove Nagara platform configs
    Nagara platform was ported to 5.15.
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- update_defconfig.sh
+++ update_defconfig.sh
@@ -24,16 +24,13 @@ ${KERNEL_DEFCONFIG_URL}/tree/${KERNEL_DEFCONFIG_HEAD}
 EOM
 
 
-PLATFORMS="nagara columbia"
+PLATFORMS="columbia"
 
 for platform in $PLATFORMS; do \
 
     case $platform in
-    nagara)
-        SOC="sm8450";;
     columbia)
         SOC="sm6450";;
-
     esac
 
     echo "================================================="
